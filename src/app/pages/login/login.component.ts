14df103bd3ddfd61c756120330b1b658a5d2cbc2
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2020-10-31 07:51:28 -0300

    Added form login (pending do work and layout)

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { LoginModel } from './login-model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formLogin: FormGroup;
+  username: FormControl;
+  password: FormControl;
+  isPost: boolean = false;
+
+  constructor() {
+    this.createForm(new LoginModel());
+  }
 
   ngOnInit(): void {
+  }
+
+  createForm(model: LoginModel) {
+    this.username = new FormControl(model.username, Validators.required);
+    this.password = new FormControl(model.password, Validators.required);
+    this.formLogin = new FormGroup({
+      username: this.username,
+      password: this.password
+    });
+  }
+
+  onSubmit() {
+    this.isPost = true;
+    if (this.formLogin.valid) {
+      alert('TODO:POST TO BACKEND');
+    }
   }
 
 }
